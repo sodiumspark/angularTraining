28ea1c312a66fd29efed20e849464c54978f0d43
Manasvi Tyagi <manasvi.tyagi@comau.com>
2019-08-23 18:22:17 +0530

    Final Version

--- src/app/pages/employee-details/employee-details.component.ts
+++ src/app/pages/employee-details/employee-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from "@angular/forms"
 import { Route } from '@angular/compiler/src/core';
-import {Router} from '@angular/router/'
+import {Router,ActivatedRoute} from '@angular/router/'
+
 
 @Component({
   selector: 'app-employee-details',
@@ -11,13 +12,15 @@ import {Router} from '@angular/router/'
 export class EmployeeDetailsComponent implements OnInit {
 
   empData: FormGroup;
-  constructor(private fb: FormBuilder, private router:Router) { 
+  constructor(private fb: FormBuilder, private router:Router, private activatedRoute:ActivatedRoute) { 
     this.empData = this.fb.group({
       namex: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
       emailx: ['', Validators.email]
     })
 
-    
+    console.log("getCurrentNavigation ->", this.router.getCurrentNavigation)
+    console.log("activatedRoute ->", this.activatedRoute)
+
     // console.log("Name recieved " + this.router.getCurrentNavigation().extras.state.name)
     // console.log("Email recieved " + this.empData.get('emailx'))
   }
